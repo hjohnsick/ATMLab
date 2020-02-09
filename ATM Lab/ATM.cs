0e58b6a3dafcf95125a62472bdd0c02a6f94d108
heather <hjohnsick@yahoo.com>
2020-02-09 10:47:46 -0500

    ATMLab2ndAttempt

--- ATM Lab/ATM.cs
+++ ATM Lab/ATM.cs
@@ -6,10 +6,19 @@ namespace ATM_Lab
 {
     class ATM
     {
-        private string name;
-        private string password;
+        //private string name;
+        //private string password;
 
         
+
+    }
+    private static string Register(string name, string password)
+    {
+        Console.WriteLine("What is your name?");
+        name = Console.ReadLine();
+        Console.WriteLine("What is your password?");
+        password = Console.ReadLine();
+
 
     }
  
--- ATM Lab/Program.cs
+++ ATM Lab/Program.cs
@@ -13,12 +13,19 @@ namespace ATM_Lab
             {
                 "\t1.  Register", "\t2.  Login", "\t3.  Logout", "\t4.  Check Balance", "\t5.  Deposit", "\tWithdraw"
             };
-
-            
-
-
+            PrintList(atmOptions);
         }
 
-       
+        public static void PrintList(List<string> atmOptions)
+        {
+            for (int i = 0; i < atmOptions.Count; i++)//have to input the fields from the duties class
+            {
+                Console.WriteLine();
+                Console.WriteLine((i + 1) + ":");
+                Console.WriteLine($"\t{atmOptions[i]}");
+
+            }
+        }
     }
 }
+
